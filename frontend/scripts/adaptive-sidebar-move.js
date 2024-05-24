bf4ae3fb4763129e42ab60226b91c40b43f04261
A <annett-online@ya.ru>
2024-05-24 17:20:12 +0300

    layout parth of adap

---
+++ frontend/scripts/adaptive-sidebar-move.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementById("adaptive-sidebar").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("adaptive-sidebar").style.width = "0";
+}
